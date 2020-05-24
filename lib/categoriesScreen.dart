f182ff25c722a39bcab485a6672fd7a5c41f361d
Gabriel Joshua Paet <gjvpaet@gmail.com>
2020-05-24 22:34:30 +0800

    Created categories screen and made it the default page.

---
+++ lib/categoriesScreen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'categoryItem.dart';
+
+import './datas/dummyData.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'DeliMeal',
+        ),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(
+                  title: catData.title,
+                  color: catData.color,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
